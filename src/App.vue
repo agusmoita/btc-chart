7da4232afafafff13a69923a5dcaa02f4d590484
Augusto Moita <a94moita@outlook.com>
2018-08-02 13:03:38 -0300

    Uso de async await para obtener datos

--- src/App.vue
+++ src/App.vue
@@ -30,31 +30,26 @@ export default {
     setInterval(this.obtener, MINUTOS * 60 * 1000)
   },
   methods: {
-    obtener () {
-      const app = this
-      fetch('https://api.coindesk.com/v1/bpi/historical/close.json?currency=ARS')
-        .then(function (response) {
-          return response.json()
-        })
-        .then(function (data) {
-          const bpi = data.bpi
-          app.datos = {
-            labels: Object.keys(bpi).map(k => moment(k).format('DD-MM')),
-            datasets: [
-              {
-                label: 'Precio Bitcoin',
-                backgroundColor: colores.amarillo,
-                pointBackgroundColor: colores.amarillo,
-                pointBorderColor: colores.azul,
-                pointRadius: 4,
-                borderColor: colores.azul,
-                lineTension: 0.4,
-                fill: false,
-                data: Object.values(bpi).map(k => k)
-              }
-            ]
+    async obtener () {
+      const response = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
+      const data = await response.json()
+      const bpi = data.bpi
+      this.datos = {
+        labels: Object.keys(bpi).map(k => moment(k).format('DD-MM')),
+        datasets: [
+          {
+            label: 'Precio Bitcoin',
+            backgroundColor: colores.amarillo,
+            pointBackgroundColor: colores.amarillo,
+            pointBorderColor: colores.azul,
+            pointRadius: 4,
+            borderColor: colores.azul,
+            lineTension: 0.4,
+            fill: false,
+            data: Object.values(bpi).map(k => k)
           }
-        })
+        ]
+      }
     }
   }
 }
